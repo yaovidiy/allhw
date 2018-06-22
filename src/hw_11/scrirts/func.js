a4df1561b0f72ec8039a4bf1e81df65a2be9d24d
yaovidiy <yaovdiy@google.com>
2018-06-22 18:12:30 +0300

    trying to make it close others

--- src/hw_11/scrirts/func.js
+++ src/hw_11/scrirts/func.js
@@ -4,31 +4,27 @@ export function Accordion (accordions) {
      const header = accordions.querySelector('.accordion__header');
      const content = accordions.querySelector('.accordion__content');
      const active = 'accordion__content_active';
-     const activeHead = 'accordion__header_active';
      const triangleOpen = 'triangle__open';
      const triangleClose = 'triangle__close';
-     let state = 'close';
+     let activeAccordion = document.querySelector(active);
      header.addEventListener('click', function(){
-         if (state === 'open'){
-             closeAccordion();
-             state = 'close';
-         }
-         else{
-            openAccordion();
-            state = 'open';              
-         }
+            openAccordion();              
+         
      });
  
  function openAccordion() {
+     if (activeAccordion){
+     activeAccordion.classList.remove(active);    
      content.classList.add(active); //Открываем скрытый контент
-     header.classList.remove(triangleClose); // Убираем черный смотрящий влево триугольгик 
+     header.classList.remove(triangleClose); // Убираем белый смотрящий вверх триугольгик 
      header.classList.add(triangleOpen);// Добавляем белый смотрящий вниз треугольник
- }
- function closeAccordion() {
-     content.classList.remove(active); //Закрываем скрытый контент
-     header.classList.remove(triangleOpen);// Убираем белый смотрящий вниз триугольгик 
-     header.classList.add(triangleClose);// Добавляем черный смотрящий влево треугольник
-
+     }
+     else {
+        content.classList.add(active); //Открываем скрытый контент
+        header.classList.remove(triangleClose); // Убираем белый смотрящий вверх триугольгик 
+        header.classList.add(triangleOpen);// Добавляем белый смотрящий вниз треугольник            
+     }
+     
  }
  
  }
