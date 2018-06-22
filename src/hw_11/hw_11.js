1529e6a8e926abc4a377b62b25a86d093a25a616
yaovidiy <yaovdiy@google.com>
2018-06-22 14:27:54 +0300

    Сделал аккордион, открывается и закрывается все секции

--- src/hw_11/hw_11.js
+++ src/hw_11/hw_11.js
@@ -1,4 +1,8 @@
 import './hw_11.scss';
 import { Accordion } from './scrirts/func';
 
-Accordion();
+let accordiones = document.querySelectorAll('.accordion');
+for (let i = 0; i < accordiones.length; i++) {
+    Accordion(accordiones[i]);    
+}
+//Accordion();
--- src/hw_11/scrirts/func.js
+++ src/hw_11/scrirts/func.js
@@ -1,9 +1,11 @@
-export function Accordion () {
+export function Accordion (accordions) {
     // const accordion = document.querySelector('.accordion');
-     const header = document.querySelector('.accordion__header');
-     const content = document.querySelector('.accordion__content');
+     const header = accordions.querySelector('.accordion__header');
+     const content = accordions.querySelector('.accordion__content');
      const active = 'accordion__content_active';
      const activeHead = 'accordion__header_active';
+     const triangleOpen = 'triangle__open';
+     const triangleClose = 'triangle__close';
      let state = 'close';
      header.addEventListener('click', function(){
          if (state === 'open'){
@@ -17,12 +19,19 @@ export function Accordion () {
      });
  
  function openAccordion() {
-     content.classList.add(active);
-     header.classList.add(activeHead);
+     content.classList.add(active); //Открываем скрытый контент
+     header.classList.add(activeHead); //Меняем цвет header'a
+     header.classList.remove(triangleClose); // Убираем черный смотрящий влево триугольгик 
+     header.classList.add(triangleOpen);// Добавляем белый смотрящий вниз треугольник
+
+
  }
  function closeAccordion() {
-     content.classList.remove(active);
-     header.classList.remove(activeHead);
+     content.classList.remove(active); //Закрываем скрытый контент
+     header.classList.remove(activeHead);//Меняем цвет header'a
+     header.classList.remove(triangleOpen);// Убираем белый смотрящий вниз триугольгик 
+     header.classList.add(triangleClose);// Добавляем черный смотрящий влево треугольник
+
  }
  
  }
