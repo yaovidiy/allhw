9b12bd1413eaa597a107366e8a7fd726de8d0a32
yaovidiy <yaovdiy@google.com>
2018-06-23 11:35:05 +0300

    улучшил код, сделал миксины и переменные

--- src/hw_11/scrirts/func.js
+++ src/hw_11/scrirts/func.js
@@ -1,23 +1,21 @@
 export function Accordion (accordions) {
-    // const accordion = document.querySelector('.accordion');
-   // const sections = Array.from(accordions.querySelectorAll('.accordion__section'));
      const header = accordions.querySelector('.accordion__header');
      const content = accordions.querySelector('.accordion__content');
      const active = 'accordion__content_active';
      const triangleOpen = 'triangle__open';
      const triangleClose = 'triangle__close';
      header.addEventListener('click', function(){
-            openAccordion();              
-         
+            openAccordion();                   
      });
- 
  function openAccordion() {
-    let activeAccordion = document.querySelector('.accordion__content_active');
-     activeAccordion.classList.remove(active);    
+    let activeAccordion = document.querySelector('.accordion__content_active');//Ищем активный элемент
+    let activePerent = activeAccordion.parentElement;//Выходим на его родителя accordion__section
+    let activeHeader = activePerent.children[0];//Выбираем первый дочерний элемент accorion__header
+    activeHeader.classList.remove(triangleOpen);//Убираем у активного иконку открытого треуголнька
+    activeHeader.classList.add(triangleClose);//Добавляем закрытый треугольник
+     activeAccordion.classList.remove(active);//Закрываем открытый элемент
      content.classList.add(active); //Открываем скрытый контент
      header.classList.remove(triangleClose); // Убираем белый смотрящий вверх триугольгик 
      header.classList.add(triangleOpen);// Добавляем белый смотрящий вниз треугольник
      }
-     
- 
  }
