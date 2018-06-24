e87a1565ff2caabca2fcfbc999cb87ad438f8c1a
yaovidiy <yaovdiy@google.com>
2018-06-24 12:07:43 +0300

    remove unnessesery thinks made js works right

--- src/hw_11/scrirts/main.js
+++ src/hw_11/scrirts/main.js
@@ -5,21 +5,27 @@ export function accordion (accordion) {
     const triangleClose = 'triangle__close';
     let activeAcordion;
     let activingAcordion;
+    let activeHeader;
     for (let i = 0; i < header.length;i++){
         header[i].addEventListener('click', function(){
             let section = header[i].parentElement;
             activeAcordion = accordion.querySelector('.accordion__content_active');
+            activeHeader = accordion.querySelector('.triangle__open');            
             activingAcordion = section.children[1];
             if (activeAcordion === activingAcordion){
+                header[i].classList.remove(triangleOpen);
                 header[i].classList.add(triangleClose);
                 activingAcordion.classList.remove(active);
             }
             else if (activeAcordion === null) {
+                header[i].classList.remove(triangleClose);
                 header[i].classList.add(triangleOpen);
                 activingAcordion.classList.add(active);
             }
             else {
                 activeAcordion.classList.remove(active);
+                activeHeader.classList.remove(triangleOpen);
+                activeHeader.classList.add(triangleClose);
                 header[i].classList.add(triangleOpen);
                 activingAcordion.classList.add(active);
             }
